c74ab0ece22e1cb061092a326813bac3f25644f2
Silas Oliveira <silas.spo@hotmail.com>
2021-02-02 10:37:48 -0300

    projeto-sliderShow-finalizado

---
+++ script.js
@@ -0,0 +1,36 @@
+let totalSlider = document.querySelectorAll('.slider--item').length;
+let currentSlider = 0
+let sliderWidth = document.querySelector('.slider').clientWidth
+
+document.querySelector('.slider--width').style.width = `${sliderWidth * totalSlider}px `    
+
+document.querySelector('.slider--controls').style.width = `${sliderWidth}px `
+
+document.querySelector('.slider--controls').style.height = `${document.querySelector('.slider').clientHeight}px `
+
+function goPrev(){
+    currentSlider --
+    if(currentSlider < 0) {
+        currentSlider = totalSlider - 1
+    }
+    updadeMargin()
+}
+
+function goNext(){
+    currentSlider ++
+   
+    if(currentSlider > (totalSlider -1)){
+        currentSlider = 0
+        
+    }
+    updadeMargin()
+}
+
+function updadeMargin(){
+    let sliderItemWidth = document.querySelector('.slider--item').clientWidth
+    let newMargin = (currentSlider * sliderItemWidth)
+    document.querySelector('.slider--width').style.marginLeft = `-${newMargin}px`
+   
+}
+
+setInterval(goNext,4000)
